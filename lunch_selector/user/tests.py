c31cc245fb7998723d070e718d1c71d3c0e37e4d
Melon <m.melon@m2amedia.tv>
2021-07-18 16:21:46 +0600

    Added more test and refactor user serializer tests

--- lunch_selector/user/tests.py
+++ lunch_selector/user/tests.py
@@ -1,4 +1,6 @@
 """user app test cases"""
+import copy
+
 from django.test import TestCase
 from rest_framework.exceptions import ValidationError
 
@@ -17,10 +19,23 @@ class SelectorUserModelTest(TestCase):
 
 class UserSerializerTest(TestCase):
 
-    def test_without_password(self):
-        serializer = UserSerializer(data={"username": "test_user"})
-        self.assertEqual(serializer.is_valid(), False)
-        self.assertRaises(ValidationError, serializer.is_valid, raise_exception=True)
+    def test_required_fields(self):
+        fields = ["username", "password", "confirm_password", "user_type"]
+        valid_data = {
+            "username": "test_user",
+            "password": "h@rd-p@$$w0rd",
+            "confirm_password": "h@rd-p@$$w0rd",
+            "user_type": "admin"
+        }
+        for field in fields:
+            _data = copy.deepcopy(valid_data)
+            _data.pop(field)
+            serializer = UserSerializer(data=_data)
+            self.assertEqual(serializer.is_valid(), False)
+            self.assertRaisesRegex(
+                ValidationError, f"{f'{field}': [ErrorDetail(string='This field is required.', code='required')]}",
+                serializer.is_valid, raise_exception=True
+            )
 
     def test_invalid_password(self):
         invalid_passwords = {
@@ -48,7 +63,8 @@ class UserSerializerTest(TestCase):
             data={
                 "username": "test_user",
                 "password": "th1s-1s-h@rd-p@$$w0rd",
-                "confirm_password": "not-following-password"
+                "confirm_password": "not-following-password",
+                "user_type": "admin"
             }
         )
         self.assertEqual(serializer.is_valid(), False)
@@ -62,7 +78,8 @@ class UserSerializerTest(TestCase):
             data={
                 "username": "test_user",
                 "password": "h@rd-p@$$w0rd",
-                "confirm_password": "h@rd-p@$$w0rd"
+                "confirm_password": "h@rd-p@$$w0rd",
+                "user_type": "employee"
             }
         )
         self.assertEqual(serializer.is_valid(), True)
