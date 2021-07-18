87a82116b35c2b6614a459e3663327cb943b2b22
Melon <m.melon@m2amedia.tv>
2021-07-18 09:46:12 +0600

    update createsuperuser command

--- lunch_selector/user/models.py
+++ lunch_selector/user/models.py
@@ -1,14 +1,22 @@
 """Models for user and user management"""
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    """Custom user manager to set admin type for superuser"""
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        """Override default createsuperuser command"""
+        extra_fields.setdefault("user_type", SelectorUser.ADMIN)
+        return super().create_superuser(username, email, password, **extra_fields)
 
 
 class SelectorUser(AbstractUser):
     """Custom model for additional fields"""
-    ADMIN = "ADMIN"
-    EMPLOYEE = "EMPLOYEE"
-    RESTAURANT_MANAGER = "RESTAURANT_MANAGER"
+    ADMIN = "admin"
+    EMPLOYEE = "employee"
+    RESTAURANT_MANAGER = "restaurant_manager"
 
     USER_TYPES = (
         (ADMIN, _("Admin")),
@@ -16,3 +24,6 @@ class SelectorUser(AbstractUser):
         (RESTAURANT_MANAGER, _("Restaurant manager")),
     )
     user_type = models.CharField(verbose_name=_("User Type"), max_length=30, choices=USER_TYPES)
+
+    objects = CustomUserManager()
+    REQUIRED_FIELDS = []
