71190f3cfb88dbcc039b43b01497b551ea5224ab
Melon <m.melon@m2amedia.tv>
2021-07-18 09:11:03 +0600

    update DJANGO_SETTINGS_MODULE

--- lunch_selector/lunch_selector/wsgi.py
+++ lunch_selector/lunch_selector/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MeanuSelector.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lunch_selector.settings")
 
 application = get_wsgi_application()
