b4a5848f30210f27f339c986bba622ab7a336a67
Melon <m.melon@m2amedia.tv>
2021-07-18 08:25:40 +0600

    Added custom user model

--- MeanuSelector/user/models.py
+++ MeanuSelector/user/models.py
@@ -1,3 +1,18 @@
+"""Models for user and user management"""
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class MenuSelectorUser(AbstractUser):
+    """Custom model for additional fields"""
+    ADMIN = "ADMIN"
+    EMPLOYEE = "EMPLOYEE"
+    RESTAURANT_MANAGER = "RESTAURANT_MANAGER"
+
+    USER_TYPES = (
+        (ADMIN, _("Admin")),
+        (EMPLOYEE, _("Employee")),
+        (RESTAURANT_MANAGER, _("Restaurant manager")),
+    )
+    user_type = models.CharField(verbose_name=_("User Type"), choices=USER_TYPES)
