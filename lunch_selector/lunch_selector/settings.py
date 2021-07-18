69ce7a796b5e468172af0f6c85a6242eab7aae21
Melon <m.melon@m2amedia.tv>
2021-07-18 11:32:31 +0600

    added auth model to settings.py

--- lunch_selector/lunch_selector/settings.py
+++ lunch_selector/lunch_selector/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "user"
+    "user",
+    "rest_framework.authtoken"
 ]
 AUTH_USER_MODEL = "user.SelectorUser"
 
