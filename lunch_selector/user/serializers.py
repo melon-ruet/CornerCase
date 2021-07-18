f1c33b2ba2913dc229ae9544c7044e5e9fdd3c50
Melon <m.melon@m2amedia.tv>
2021-07-18 12:16:18 +0600

    user serializer added with tests

---
+++ lunch_selector/user/serializers.py
@@ -0,0 +1,69 @@
+"""User related serializers"""
+import logging
+
+from django.contrib.auth import password_validation as validators, get_user_model
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import serializers
+from rest_framework.utils.representation import smart_repr
+
+from .models import SelectorUser
+
+logger = logging.getLogger(__name__)
+UserModel = get_user_model()
+
+
+class PasswordValidator:
+    """Password match validator"""
+    message = _("{password_field} should be equal to {confirm_password_field}.")
+
+    def __init__(self, password_field="password", confirm_password_field="confirm_password",
+                 message=None):
+        self.password_field = password_field
+        self.confirm_password_field = confirm_password_field
+        self.message = message or self.message
+
+    def __call__(self, attrs):
+        if attrs[self.password_field] != attrs[self.confirm_password_field]:
+            message = self.message.format(
+                password_field=self.password_field,
+                confirm_password_field=self.confirm_password_field,
+            )
+            raise serializers.ValidationError(message, code="password_mismatch")
+
+    def __repr__(self):
+        return "<%s(password_field=%s, confirm_password_field=%s)>" % (
+            self.__class__.__name__,
+            smart_repr(self.password_field),
+            smart_repr(self.confirm_password_field)
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """UserSerializer for creating user with a password"""
+    password_max_len = UserModel._meta.get_field("password").max_length
+    password = serializers.CharField(
+        label=_("Password"), max_length=password_max_len, write_only=True
+    )
+    confirm_password = serializers.CharField(
+        label=_("Confirm password"), max_length=password_max_len, write_only=True
+    )
+
+    def validate_password(self, value):
+        """Validate password value with default password validators"""
+        logger.info("Validating user password")
+        validators.validate_password(password=value)
+        return value
+
+    def create(self, validated_data):
+        """Create user after validation all"""
+        validated_data.pop("confirm_password")
+        user = UserModel.objects.create_user(**validated_data)
+        logger.info("User created: %s", user.username)
+        return user
+
+    class Meta:
+        model = SelectorUser
+        validators = [PasswordValidator()]
+        fields = (
+            "id", "username", "first_name", "last_name", "email", "password", "confirm_password"
+        )
--- lunch_selector/user/tests.py
+++ lunch_selector/user/tests.py
@@ -1,7 +1,9 @@
 """user app test cases"""
 from django.test import TestCase
+from rest_framework.exceptions import ValidationError
 
 from .models import SelectorUser
+from .serializers import UserSerializer
 
 
 class SelectorUserModelTest(TestCase):
@@ -11,3 +13,57 @@ class SelectorUserModelTest(TestCase):
             user_type=SelectorUser.ADMIN
         )
         self.assertEqual(user.user_type, user.ADMIN)
+
+
+class UserSerializerTest(TestCase):
+
+    def test_without_password(self):
+        serializer = UserSerializer(data={"username": "test_user"})
+        self.assertEqual(serializer.is_valid(), False)
+        self.assertRaises(ValidationError, serializer.is_valid, raise_exception=True)
+
+    def test_invalid_password(self):
+        invalid_passwords = {
+            "test": ["This password is too short", "This password is too common"],
+            "1234": ["This password is too short", "This password is too common"],
+            "hello": ["This password is too short", "This password is too common"],
+            "helloworld": ["This password is too common"],
+            "": ["This field may not be blank."]
+        }
+
+        for password, errors in invalid_passwords.items():
+            serializer = UserSerializer(
+                data={"username": "test_user", "password": password, "confirm_password": password}
+            )
+            self.assertEqual(serializer.is_valid(), False)
+
+            for error in errors:
+                self.assertRaisesRegex(
+                    ValidationError, error,
+                    serializer.is_valid, raise_exception=True
+                )
+
+    def test_match_password(self):
+        serializer = UserSerializer(
+            data={
+                "username": "test_user",
+                "password": "th1s-1s-h@rd-p@$$w0rd",
+                "confirm_password": "not-following-password"
+            }
+        )
+        self.assertEqual(serializer.is_valid(), False)
+        self.assertRaisesRegex(
+            ValidationError, "password should be equal to confirm_password",
+            serializer.is_valid, raise_exception=True
+        )
+
+    def test_create_user(self):
+        serializer = UserSerializer(
+            data={
+                "username": "test_user",
+                "password": "h@rd-p@$$w0rd",
+                "confirm_password": "h@rd-p@$$w0rd"
+            }
+        )
+        self.assertEqual(serializer.is_valid(), True)
+        serializer.save()
