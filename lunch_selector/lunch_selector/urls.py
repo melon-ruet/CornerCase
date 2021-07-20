12c8fb3d64a22325823856ec508e401f75d48afc
Melon <m.melon@m2amedia.tv>
2021-07-20 10:36:24 +0600

    refactor urls

--- lunch_selector/lunch_selector/urls.py
+++ lunch_selector/lunch_selector/urls.py
@@ -1,6 +1,6 @@
 """lunch_selector URL Configuration"""
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -16,7 +16,7 @@ schema_view = get_schema_view(
       description="Endpoints of Lunch Selector application",
    ),
    public=True,
-   permission_classes=(permissions.AllowAny,),
+   permission_classes=(permissions.AllowAny,)
 )
 
 urlpatterns = [
