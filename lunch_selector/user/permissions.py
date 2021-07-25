fbc084fce9a3c63ba7e35c61aed05f9b4a7c8c24
Melon <m.melon@m2amedia.tv>
2021-07-25 12:58:21 +0600

    Custom django model permission added for view permission

--- lunch_selector/user/permissions.py
+++ lunch_selector/user/permissions.py
@@ -1,10 +1,25 @@
 """All DRF permissions for different level of users"""
 from rest_framework import permissions
+from rest_framework.permissions import DjangoModelPermissions
 
 
-class SuperuserPermission(permissions.BasePermission):
+class SuperuserPermission(permissions.IsAuthenticated):
     """superuser permission checker class"""
 
     def has_permission(self, request, view):
         """chek superuser permission"""
-        return request.user.is_superuser
+        has_perm = super().has_permission(request, view)
+        return has_perm and request.user.is_superuser
+
+
+class CustomDjangoModelPermissions(DjangoModelPermissions):
+    """Add model view permission"""
+    perms_map = {
+        "GET": ["%(app_label)s.view_%(model_name)s"],
+        "OPTIONS": [],
+        "HEAD": [],
+        "POST": ["%(app_label)s.add_%(model_name)s"],
+        "PUT": ["%(app_label)s.change_%(model_name)s"],
+        "PATCH": ["%(app_label)s.change_%(model_name)s"],
+        "DELETE": ["%(app_label)s.delete_%(model_name)s"],
+    }
