69856c69c14151b9992267f0f3a295c108156b0a
Melon <m.melon@m2amedia.tv>
2021-07-25 23:08:27 +0600

    test runner refactor

--- lunch_selector/lunch_selector/test_runner.py
+++ lunch_selector/lunch_selector/test_runner.py
@@ -1,10 +1,14 @@
+"""Custom test runner"""
 from django.test.runner import DiscoverRunner
 
 from user.management.commands.create_groups import Command
 
 
 class CustomTestRunner(DiscoverRunner):
+    """Custom runner for populating groups"""
+
     def setup_databases(self, **kwargs):
+        """DB setup of test"""
         _return = super().setup_databases(**kwargs)
         Command().handle()
         return _return
