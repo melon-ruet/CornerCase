73ca240c4c0408a49fd595cfde9e1508a8b5ffda
Melon <m.melon@m2amedia.tv>
2021-07-25 21:33:03 +0600

    refactor import test runner

--- lunch_selector/lunch_selector/test_runner.py
+++ lunch_selector/lunch_selector/test_runner.py
@@ -1,9 +1,10 @@
 from django.test.runner import DiscoverRunner
 
+from user.management.commands.create_groups import Command
+
 
 class CustomTestRunner(DiscoverRunner):
     def setup_databases(self, **kwargs):
         _return = super().setup_databases(**kwargs)
-        from user.management.commands.create_groups import Command
         Command().handle()
         return _return
