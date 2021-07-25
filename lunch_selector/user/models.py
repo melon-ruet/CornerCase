42e57d7be50cf30e13768ffffe3f881f9f94ec3d
Melon <m.melon@m2amedia.tv>
2021-07-25 12:57:30 +0600

    add user to group while creating user

--- lunch_selector/user/models.py
+++ lunch_selector/user/models.py
@@ -1,15 +1,30 @@
 """Models for user and user management"""
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser, UserManager, Group
 
 
 class CustomUserManager(UserManager):
     """Custom user manager to set admin type for superuser"""
+
+    @staticmethod
+    def _add_to_group(user):
+        """Add user to group"""
+        group = Group.objects.get_by_natural_key(user.user_type)
+        user.groups.add(group)
+
     def create_superuser(self, username, email=None, password=None, **extra_fields):
         """Override default createsuperuser command"""
         extra_fields.setdefault("user_type", SelectorUser.ADMIN)
-        return super().create_superuser(username, email, password, **extra_fields)
+        user = super().create_superuser(username, email, password, **extra_fields)
+        self._add_to_group(user)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        """Override to add user to group"""
+        user = super().create_user(username, email, password, **extra_fields)
+        self._add_to_group(user)
+        return user
 
 
 class SelectorUser(AbstractUser):
