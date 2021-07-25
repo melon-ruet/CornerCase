5c4f06391cbec5b18f8ce3f8452be8d90ddb02fc
Melon <m.melon@m2amedia.tv>
2021-07-25 11:45:51 +0600

    create user groups

---
+++ lunch_selector/user/management/commands/create_groups.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import Group, Permission
+from django.core.management.base import BaseCommand
+
+from user.models import SelectorUser
+
+
+class Command(BaseCommand):
+    help = "Create or update user groups"
+
+    def handle(self, *args, **options):
+        groups = {
+            SelectorUser.ADMIN: {},
+            SelectorUser.RESTAURANT_MANAGER: {
+                "add_restaurant",
+                "change_restaurant",
+                "delete_restaurant",
+                "view_restaurant",
+                "add_menu",
+                "change_menu",
+                "delete_menu",
+                "view_menu",
+            },
+            SelectorUser.EMPLOYEE: {
+                "view_menu",
+                "view_menuvote",
+                "add_menuvote",
+                "delete_menuvote",
+                "change_menuvote",
+            }
+        }
+
+        for name, permissions in groups.items():
+            permissions = Permission.objects.filter(
+                codename__in=permissions
+            )
+            group, _ = Group.objects.update_or_create(
+                **{"name": name}
+            )
+            group.permissions.add(*permissions)
+
+            self.stdout.write(
+                self.style.SUCCESS(f"Successfully created/updated groups {name}")
+            )
