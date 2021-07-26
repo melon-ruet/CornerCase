e286a20d6914b34c8c8a9ead9d9f6aad5576ee76
Melon <m.melon@m2amedia.tv>
2021-07-26 16:17:26 +0600

    added restaurant view logics

--- lunch_selector/restaurant/views.py
+++ lunch_selector/restaurant/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+"""Restaurant related views"""
+import logging
 
-# Create your views here.
+from rest_framework import viewsets
+
+from restaurant.serializers import RestaurantSerializer
+from user.models import SelectorUser
+
+logger = logging.getLogger(__name__)
+
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    """Restaurant create, update, delete DRF views"""
+    serializer_class = RestaurantSerializer
+
+    def get_queryset(self):
+        objects = self.serializer_class.Meta.model.objects
+        if self.request.user.user_type == SelectorUser.RESTAURANT_MANAGER:
+            return objects.filter(manager=self.request.user.id)
+        return objects.all()
+
+    def initial(self, request, *args, **kwargs):
+        """Add initial value before create or update"""
+        user_type = getattr(request.user, "user_type", None)
+        if user_type == SelectorUser.RESTAURANT_MANAGER:
+            request.data["manager"] = request.user.id
+        super().initial(request, *args, **kwargs)
