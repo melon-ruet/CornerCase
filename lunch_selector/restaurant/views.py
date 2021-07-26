8073dfa34356464229586a31458723753feb1b3c
Melon <m.melon@m2amedia.tv>
2021-07-26 16:27:34 +0600

    Added menu view to crud menus

--- lunch_selector/restaurant/views.py
+++ lunch_selector/restaurant/views.py
@@ -1,9 +1,11 @@
 """Restaurant related views"""
+import datetime
 import logging
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-from restaurant.serializers import RestaurantSerializer
+from restaurant.serializers import RestaurantSerializer, MenuSerializer
 from user.models import SelectorUser
 
 logger = logging.getLogger(__name__)
@@ -14,6 +16,7 @@ class RestaurantViewSet(viewsets.ModelViewSet):
     serializer_class = RestaurantSerializer
 
     def get_queryset(self):
+        """get restaurants based on user type"""
         objects = self.serializer_class.Meta.model.objects
         if self.request.user.user_type == SelectorUser.RESTAURANT_MANAGER:
             return objects.filter(manager=self.request.user.id)
@@ -25,3 +28,35 @@ class RestaurantViewSet(viewsets.ModelViewSet):
         if user_type == SelectorUser.RESTAURANT_MANAGER:
             request.data["manager"] = request.user.id
         super().initial(request, *args, **kwargs)
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    """Menu create, update, delete DRF views"""
+    serializer_class = MenuSerializer
+
+    def get_queryset(self):
+        """Get menus by user type"""
+        objects = self.serializer_class.Meta.model.objects
+        user_type = getattr(self.request.user, "user_type", None)
+        if user_type == SelectorUser.EMPLOYEE:
+            return objects.filter(
+                day=datetime.datetime.today()
+            )
+        elif user_type == SelectorUser.RESTAURANT_MANAGER:
+            return objects.filter(restaurant__manager=self.request.user)
+        return objects.all()
+
+    def create(self, request, *args, **kwargs):
+        """Override to check if proper restaurant id provided"""
+        if request.user.user_type == SelectorUser.RESTAURANT_MANAGER:
+            restaurants = RestaurantSerializer.Meta.model.objects.filter(
+                manager=request.user
+            )
+            if request.data.get("restaurant", None) not in \
+                    restaurants.values_list("id", flat=True):
+                return Response(
+                    "Invalid restaurant ID",
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        return super().create(request, *args, **kwargs)
