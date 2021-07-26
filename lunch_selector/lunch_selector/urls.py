187f3f8ba57f071fb03c5368d78415a81f266449
Melon <m.melon@m2amedia.tv>
2021-07-26 18:32:36 +0600

    refactored base url file

--- lunch_selector/lunch_selector/urls.py
+++ lunch_selector/lunch_selector/urls.py
@@ -1,5 +1,4 @@
 """lunch_selector URL Configuration"""
-# from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -7,20 +6,20 @@ from rest_framework import permissions
 
 from rest_framework.authtoken.views import obtain_auth_token
 
-from user.views import UserCreateView
-
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Lunch Selector API",
-      default_version="v1",
-      description="Endpoints of Lunch Selector application",
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,)
+    openapi.Info(
+        title="Lunch Selector API",
+        default_version="v1",
+        description="Endpoints of Lunch Selector application",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
 )
 
 urlpatterns = [
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("token/", obtain_auth_token),
-    path("user/", UserCreateView.as_view()),
+    path("user/", include("user.urls")),
+    path("restaurants/", include("restaurant.urls")),
+    path("votes", include("vote.urls")),
 ]
